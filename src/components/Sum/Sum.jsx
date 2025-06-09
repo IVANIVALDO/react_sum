ed3aee17f27fb882a76971c2063bb8ce11472731
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-06-09 01:39:37 -0300

    Implementa componente Sum com testes passando

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,21 +1,7 @@
-// export const Sum = () => ();
-// Corrigido:
 import React from 'react';
 
-export function Sum({ a = 0, b = 0 }) {
-  const sum = a + b;
+const Sum = ({ a = 0, b = 0 }) => {
+  return <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>;
+};
 
-  return (
-    <p>
-      Sum of {a} and {b} is {sum}
-    </p>
-  );
-}
-
-<>
-  <Sum a={2} b={3} />
-  <Sum a={-5} b={5} />
-  <Sum a={10} />
-  <Sum b={5} />
-  <Sum />
-</>;
+export default Sum;
